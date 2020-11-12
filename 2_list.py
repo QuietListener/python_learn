175201ac2142b2b5e942b016e31ba36c487b0d24
junjun <junjun@baicizhan.com>
2020-11-12 18:12:34 +0800

    fx

--- 2_list.py
+++ 2_list.py
@@ -120,3 +120,41 @@ print(ll4_3)
 
 ll4_4 = ll4[-2:] #最后2个
 print(ll4_4)
+ll4[-1] = 'g' #说明是复制的
+print(ll4)
+print(ll4_4)
+
+#复制列表
+ll5 = ["c","d","e"]
+ll5_1 = ll5[:]
+ll5_2 = ll5
+ll5[0] = 'k'
+print(ll5)
+print(ll5_1)
+print(ll5_2)
+
+
+#元组 元素是不可变的
+#tuple 是一种有序列表；tuple和list非常类似，但是tuple一旦初始化就不能修改，比如同样是列出同学的名字
+#不可变的tuple有什么意义？因为tuple不可变，所以代码更安全。如果可能，能用tuple代替list就尽量用tuple。
+t1 = (1,2)
+print(t1)
+t2 = () #空tuple
+print(t2)
+
+#特变注意下面不是一个tuple
+t3 = (3) #这里有一个歧义 小括号可以是tuple也可以是数学上的小括号，python在这里默认取数学上的小括号
+print(t3) #3
+
+#一个元素的tuple
+t4 = (3,)
+print(t4)
+print(len(t4))
+
+
+print("\n---改变tuple---")
+t11 = (1,2,[True,3])
+print(t11) #(1, 2, [True, 3])
+t11[2][1] = False
+print(t11) #(1, 2, [True, False])
+#----tuple的不便性其实是每个元素指向的内存不便，内存的内容可以变的---
